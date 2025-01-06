48f8110ff4add7d4797d04ec3e2d57a9da74d3e5
Andrey Zakatov <andreyzakatov@MacBook-Air-Andrey.local>
2025-01-06 14:21:18 +0300

    Выполнено задание модуля

---
+++ LinuxIntro/mysite/shopapp/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+app_name = 'shopapp'
+
+urlpatterns = [
+    path('', views.shop_apps, name='product_list'),
+]
---
+++ LinuxIntro/mysite/shopapp/views.py
@@ -0,0 +1,33 @@
+from timeit import default_timer
+
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+
+def shop_apps(request: HttpRequest):
+    PRODUCTS = [
+        {"name": "MacBook", "type": "laptop", "price": 2999},
+        {"name": "ThinkPad", "type": "laptop", "price": 799},
+        {"name": "MacBook Pro", "type": "laptop", "price": 3999},
+        {"name": "IPhone 14 Pro", "type": "smartphone", "price": 999},
+        {"name": "IPhone xr", "type": "smartphone", "price": 200},
+        {"name": "Xiaomi m3t590pro super puper max XLL", "type": "smartphone", "price": 2000},
+        {"name": "PC intel 10500f", "type": "desktop", "price": 670},
+        {"name": "RTX 4090 TI", "type": "desktop", "price": 1670},
+    ]
+
+    device_type = request.GET.get("type")
+    min_price = request.GET.get("min_price")
+    max_price = request.GET.get("max_price")
+
+    filter_product = PRODUCTS
+
+    if device_type:
+        filter_product = [product for product in PRODUCTS if product["type"] == device_type]
+
+    if min_price:
+        filter_product = [product for product in PRODUCTS if product["price"] >= int(min_price)]
+
+    if min_price:
+        filter_product = [product for product in PRODUCTS if product["price"] <= int(max_price)]
+
+    return render(request, "shopapp/first_page.html", {"products": filter_product})
