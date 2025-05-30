c90b949507ab7e092062272585557d474fb28019
azakatov <azakatov@cspfmba.ru>
2025-05-30 14:05:07 +0300

    выполнено задание модуля

--- CBV/mysite/shopapp/urls.py
+++ CBV/mysite/shopapp/urls.py
@@ -1,12 +1,32 @@
 from django.urls import path
 
-from .views import shop_index, groups_list, products_list, orders_list
+from .views import (
+    GroupListView,
+    ShopIndexView,
+    ProductDetailsView,
+    ProductsListView,
+    OrderListView,
+    OrderDetailView,
+    ProductCreateView,
+    ProductUpdateView,
+    ProductDeleteView,
+    OrderUpdateView,
+    OrderArchivedView, OrderCreateView,
+)
 
 app_name = "shopapp"
 
 urlpatterns = [
-    path("", shop_index, name="index"),
-    path("groups/", groups_list, name="groups_list"),
-    path("products/", products_list, name="products_list"),
-    path("orders/", orders_list, name="orders_list"),
+    path("", ShopIndexView.as_view(), name="index"),
+    path("groups/", GroupListView.as_view(), name="groups_list"),
+    path("products/", ProductsListView.as_view(), name="products_list"),
+    path("products/create", ProductCreateView.as_view(), name="product_create"),
+    path("products/<int:pk>/", ProductDetailsView.as_view(), name="product_details"),
+    path("products/<int:pk>/update/", ProductUpdateView.as_view(), name="product_update"),
+    path("products/<int:pk>/archived/", ProductDeleteView.as_view(), name="product_delete"),
+    path("orders/", OrderListView.as_view(), name="order_list"),
+    path("orders/<int:pk>", OrderDetailView.as_view(), name='order_details'),
+    path("orders/<int:pk>/update", OrderUpdateView.as_view(), name='order_update'),
+    path("order/<int:pk>/archived/", OrderArchivedView.as_view(), name='order_archived'),
+    path("order/create/", OrderCreateView.as_view(), name="order_create"),
 ]
