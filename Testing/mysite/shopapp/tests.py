1fda4f193ff8ab37f5d934c22ec2deb5d9ec51a8
azakatov <azakatov@cspfmba.ru>
2025-06-05 11:26:22 +0300

    test

--- Testing/mysite/shopapp/tests.py
+++ Testing/mysite/shopapp/tests.py
@@ -1,3 +1,88 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from django.conf import settings
+from string import ascii_letters
+from random import choices
+
+from shopapp.models import Product
+
+
+class ProductCreateViewTestCase(TestCase):
+    def setUp(self):
+        self.product_name = "".join(choices(ascii_letters, k=10))
+        Product.objects.filter(name=self.product_name)
+    def test_create_product(self):
+        response = self.client.post(
+            reverse("shopapp:product_create"),
+            {
+                "name": self.product_name,
+                "price": "123.33",
+                "description": "God table",
+                "discount": "10",
+            }
+        )
+        self.assertRedirects(response, reverse("shopapp:product_list"))
+        self.assertTrue(
+            Product.objects.filter(name=self.product_name)
+        )
+
+
+class ProductdetailsViewTestCase(TestCase):
+    def setUpClass(cls):
+        cls.product = Product.objects.create(name="Best")
+
+    def tearDownClass(cls):
+        cls.product.delete()
+
+    def test_get_product(self):
+        response = self.client.get(
+            reverse("shopapp:product_details", kwargs={"pk": self.product.pk})
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_product_and_check_product(self):
+        response = self.client.get(
+            reverse("shopapp:product_details", kwargs={"pk": self.product.pk})
+        )
+        self.assertContains(response, self.product.name)
+
+
+class ProductListViewTestCase(TestCase):
+    fixtures = [
+        "products-fixture.json",
+    ]
+
+    def test_products(self):
+        response = self.client.get(reverse("shopapp:products_list"))
+        self.assertQuerysetEqual(
+            qs=Product.objects.filter(archived=False).all(),
+            values=(p.pk for p in response.context["products"]),
+            transform=lambda p: p.pk,
+        )
+        self.assertTemplateUsed(response, 'shopapp/products-list.html')
+
+
+class OrdersListViewTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.credentials = dict(username="Bob", password="1234567890987654321")
+        cls.user = User.objects.create_user(**cls.credentials)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.user.delete()
+
+    def setUp(self):
+        self.client.force_login(self.user)
+
+    def test_orders_view(self):
+        response = self.client.get(reverse("shopapp:orders_list"))
+        self.assertContains(response, "Orders")
+
+    def test_orders_view_nor_authenticated(self):
+        self.client.logout()
+        response = self.client.get(reverse("shopapp:orders_list"))
+        self.assertEqual(response.status_code, 302)
+        self.assertIn(str(settings.LOGIN_URL), response.url)
