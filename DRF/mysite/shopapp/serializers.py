746c7a2008e0df30cdfe302e4dc8391b24f9ec84
Андрей Закатов <zakatov038@gmail.com>
2025-06-19 14:38:17 +0300

    Upload New File

---
+++ DRF/mysite/shopapp/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+
+from .models import Product, Order
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = (
+            "pk",
+            "name",
+            "description",
+            "price",
+            "discount",
+            "created_at",
+            "archived",
+            "preview",
+        )
+
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['id', 'products', 'created_at']
+        read_only_fields = ['user']
