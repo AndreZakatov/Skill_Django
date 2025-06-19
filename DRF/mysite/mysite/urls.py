f52d081d5cb75df928a50e8d197a97fcc408cad2
Андрей Закатов <zakatov038@gmail.com>
2025-06-19 14:37:29 +0300

    Update urls.py

--- DRF/mysite/mysite/urls.py
+++ DRF/mysite/mysite/urls.py
@@ -1,28 +1,23 @@
-"""mysite URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.i18n import i18n_patterns
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('shop/', include('shopapp.urls')),
-    path('myauth/', include('myauth.urls')),
+    path('i18//', include('django.conf.urls.i18n')),
+    path('api/', include('myapiapp.urls')),
 ]
+
+
+urlpatterns += i18n_patterns(
+    path('admin/', admin.site.urls),
+    path('myauth/', include('myauth.urls')),
+    path('shop/', include('shopapp.urls')),
+
+)
+
 
 if settings.DEBUG:
     urlpatterns.extend(
@@ -32,3 +27,4 @@ if settings.DEBUG:
     urlpatterns.extend(
         static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     )
+
